e9bc3999178a3b6352563c30f6fb6c1bb5a25cd5
yuchen.y.bu <yuchen.y.bu@benq.com>
2018-01-25 10:38:46 +0800

    add EntryComponents 动态组件加载

---
+++ EntryComponents/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import {
+  Component, ViewChild, ViewContainerRef, ComponentFactory,
+  ComponentRef, ComponentFactoryResolver, OnDestroy
+} from '@angular/core';
+import { AlertComponent } from './exe-alert.component';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <ng-template #alertContainer></ng-template>
+    <button (click)="createComponent('success')">Create success alert</button>
+    <button (click)="createComponent('danger')">Create danger alert</button>
+  `
+})
+export class AppComponent implements OnDestroy {
+  componentRef: ComponentRef<AlertComponent>;
+
+  @ViewChild("alertContainer", { read: ViewContainerRef }) container: ViewContainerRef;
+
+  constructor(private resolver: ComponentFactoryResolver) { }
+
+  createComponent(type: string) {
+    //this.container.clear();
+    const factory: ComponentFactory<AlertComponent> =
+      this.resolver.resolveComponentFactory(AlertComponent);
+    this.componentRef = this.container.createComponent(factory);
+    this.componentRef.instance.type = type;
+     this.componentRef.instance.output.subscribe((msg: string) => console.log(msg));
+  }
+
+  ngOnDestroy() {
+    this.componentRef.destroy()
+  }
+}
